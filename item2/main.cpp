f73adfad19a5fa8948f46b95216e5adc4a9f76c6
paul <tinyshine@yeah.net>
2018-04-26 14:46:12 +0800

    add ch1

--- item2/main.cpp
+++ item2/main.cpp
@@ -44,6 +44,25 @@ void demo3() {
     cout << "fun2: " << type_id_with_cvr<decltype(func2)>().pretty_name() << endl;
 }
 
+/* -----initializer_list----- */
+template<typename T>
+void f(std::initializer_list<T> initList) {
+    cout << "T: " << type_id_with_cvr<T>().pretty_name() << endl;
+    cout << "initList: " << type_id_with_cvr<decltype(initList)>().pretty_name() << endl;
+}
+
+void demo4() {
+    auto x1 = 27;
+    auto x2(27);
+    auto x3 = {27};
+    auto x4{27};   // Note: different with "book"
+    cout << "x1: " << type_id_with_cvr<decltype(x1)>().pretty_name() << endl;
+    cout << "x2: " << type_id_with_cvr<decltype(x2)>().pretty_name() << endl;
+    cout << "x3: " << type_id_with_cvr<decltype(x3)>().pretty_name() << endl;
+    cout << "x4: " << type_id_with_cvr<decltype(x4)>().pretty_name() << endl;
+    f({11, 12, 13});
+}
+
 
 int main() {
 /*-----case 1&3----*/
@@ -51,5 +70,7 @@ int main() {
 /* -----case 2----- */
 //    demo2();
 /* -----array & functions----- */
-    demo3();
+//    demo3();
+/* -----initializer_list----- */
+//    demo4();
 }
